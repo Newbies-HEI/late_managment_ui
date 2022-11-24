604cadb8f075d526f78d742a516e1eb6105924ee
Anjaraniaina <hei.anjaraniaina@gmail.com>
2022-11-24 12:43:12 +0300

    chore: add more resources

--- src/admin/index.tsx
+++ src/admin/index.tsx
@@ -7,6 +7,9 @@ const App = () => (
   <Admin dataProvider={dataProvider}>
     <Resource name="posts" list={ListGuesser} />
     <Resource name="comments" list={ListGuesser} />
+    <Resource name="albums" list={ListGuesser}>
+    <Resource name="todos" list={ListGuesser}>
+    <Resource name="users" list={ListGuesser}>
   </Admin>
 );
 
